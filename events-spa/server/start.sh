33faec7b6ebcf2703b6f4dbd17ddad1b92f167f6
sanjdutt <sanjdutt8@gmail.com>
2021-03-26 02:51:09 +0000

    final

---
+++ events-spa/server/start.sh
@@ -0,0 +1,7 @@
+
+export SECRET_KEY_BASE=W68eso5YQOlbtvSNUR50N/HDWj6IaEhAwMR3LtzuBEQAefwYVbX84bvoTA7XtiGi
+export MIX_ENV=prod 
+export PORT=4800
+export NODEBIN='pwd'/assets/node_modules/.bin
+export PATH="$PATH:$NODEBIN"
+export DATABASE_URL=ecto://eventsspa:events1events2@45.77.107.233/events_dev
