dcd9d37862023dc6fb723a1bd3196508863e0ce7
sanjdutt <sanjdutt8@gmail.com>
2021-03-25 22:03:05 -0400

    about to deploy

--- events-spa/server/lib/events/users.ex
+++ events-spa/server/lib/events/users.ex
@@ -8,23 +8,11 @@ defmodule Events.Users do
 
   alias Events.Users.User
 
-
- def authenticate(email, password) do 
-    user = Repo.get_by(User, email: email)
-    case Argon2.check_pass(user, password) do
-      {:ok, user} -> user
-      _ -> nil
-    end
-  end
-
   @doc """
   Returns the list of users.
-
   ## Examples
-
       iex> list_users()
       [%User{}, ...]
-
   """
   def list_users do
     Repo.all(User)
@@ -32,31 +20,34 @@ defmodule Events.Users do
 
   @doc """
   Gets a single user.
-
   Raises `Ecto.NoResultsError` if the User does not exist.
-
   ## Examples
-
       iex> get_user!(123)
       %User{}
-
       iex> get_user!(456)
       ** (Ecto.NoResultsError)
-
   """
   def get_user!(id), do: Repo.get!(User, id)
 
+  def get_user_by_name!(name) do
+    Repo.get_by!(User, name: name)
+  end
+
+  def authenticate(name, pass) do
+    user = Repo.get_by!(User, name: name)
+    case Argon2.check_pass(user, pass) do
+      {:ok, user} -> user
+      _ -> nil
+    end
+  end
+
   @doc """
   Creates a user.
-
   ## Examples
-
       iex> create_user(%{field: value})
       {:ok, %User{}}
-
       iex> create_user(%{field: bad_value})
       {:error, %Ecto.Changeset{}}
-
   """
   def create_user(attrs \\ %{}) do
     %User{}
@@ -66,15 +57,11 @@ defmodule Events.Users do
 
   @doc """
   Updates a user.
-
   ## Examples
-
       iex> update_user(user, %{field: new_value})
       {:ok, %User{}}
-
       iex> update_user(user, %{field: bad_value})
       {:error, %Ecto.Changeset{}}
-
   """
   def update_user(%User{} = user, attrs) do
     user
@@ -84,15 +71,11 @@ defmodule Events.Users do
 
   @doc """
   Deletes a user.
-
   ## Examples
-
       iex> delete_user(user)
       {:ok, %User{}}
-
       iex> delete_user(user)
       {:error, %Ecto.Changeset{}}
-
   """
   def delete_user(%User{} = user) do
     Repo.delete(user)
@@ -100,14 +83,12 @@ defmodule Events.Users do
 
   @doc """
   Returns an `%Ecto.Changeset{}` for tracking user changes.
-
   ## Examples
-
       iex> change_user(user)
       %Ecto.Changeset{data: %User{}}
-
   """
   def change_user(%User{} = user, attrs \\ %{}) do
     User.changeset(user, attrs)
   end
 end
+
